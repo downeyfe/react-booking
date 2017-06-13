c962ff31b82411a0f566912b0ff91da84b415c93
Frances Downey <frances@clearscore.com>
2017-06-13 17:51:59 +0100

    Displaying booking information on page

--- src/Layout.js
+++ src/Layout.js
@@ -1,13 +1,29 @@
 /* Feel free to edit */
 import React from 'react';
+import bookings from '../bookings.json';
 
-export default function Layout({ children }) {
-  return (
-    <div className="bui-app">
-        <div className="bui-app-header">
-            {/*TODO include, or replace heading with, logo */}
-            <h1 className="bui-app-intro">Table Reservations</h1>
+export default function Layout({children}) {
+    const bookingElements = bookings[0].bookings.map((booking, index) => {
+        return (
+            <div className="booking" key={index}>
+                <h4>{booking.title} {booking.firstName} {booking.lastName}</h4>
+                <p>Time: {booking.time}</p>
+                <p>Size: {booking.partySize}</p>
+                <p>Notes: {booking.notes || 'none'}</p>
+            </div>
+        );
+    });
+
+    return (
+        <div className="bui-app">
+            <div className="bui-app-header">
+                {/*TODO include, or replace heading with, logo */}
+                <h1 className="bui-app-intro">Table Reservations</h1>
+            </div>
+
+            <div className="bookings__grid">
+                {bookingElements}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
