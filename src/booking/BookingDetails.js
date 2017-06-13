71a5b9e96e04be762325ce4a704c72d9f8aa50e7
Frances Downey <frances@clearscore.com>
2017-06-13 20:33:47 +0100

    Showing booking details, but with no functionality yet

--- src/booking/BookingDetails.js
+++ src/booking/BookingDetails.js
@@ -7,8 +7,24 @@ export default class BookingDetails extends Component {
   }
 
   render() {
-    return (
-      <div>Hi, {this.props.selected ? this.props.selected.firstName : 'no one'}</div>
-    );
+    const selected = this.props.selected;
+
+    return selected ?
+      <div>
+        <h4>Booking update</h4>
+
+        <p>Name: {selected.title} {selected.firstName} {selected.lastName}</p>
+        <p>Time: {selected.time}</p>
+        <p>Covers: {selected.partySize}</p>
+        <p>Notes: {selected.notes || 'none'}</p>
+
+        <div>
+          Seated:
+          <input type="radio" name="no" checked={!selected.seated && !selected.cancelled} />
+          <input type="radio" name="yes" checked={selected.seated && !selected.cancelled} />
+          <input type="radio" name="cancelled" checked={selected.cancelled} />
+        </div>
+      </div> :
+      null;
   }
 }
